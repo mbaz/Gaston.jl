ed408900ce097846d89b69f4297c26841681ab80
Miguel Bazdresch <eorlinga@gmail.com>
2013-03-04 21:12:38 -0500

    Complete export list

--- src/Gaston.jl
+++ src/Gaston.jl
@@ -21,7 +21,13 @@
 module Gaston
 
 export closefigure, closeall, clearfigure, figure, plot, histogram, imagesc,
-    surf, printfigure, gaston_demo, gaston_tests
+    surf, printfigure, gaston_demo, gaston_tests, set_terminal, set_filename,
+    set_default_legend, set_default_plotstyle, set_default_color,
+    set_default_marker, set_default_linewidth, set_default_pointsize,
+    set_default_title, set_default_xlabel, set_default_ylabel,
+    set_default_zlabel, set_default_box, set_default_axis, set_print_color,
+    set_print_fontface, set_print_fontsize, set_print_fontscale,
+    set_print_linewidth, set_print_size, gnuplot_send
 
 # before doing anything else, verify gnuplot is present on this system
 if !success(`which gnuplot`)
