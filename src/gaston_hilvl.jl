dc0cb5d079b3f924ce20fa0e1b48b17f8fe395a2
Miguel Bazdresch <eorlinga@gmail.com>
2013-05-04 20:59:08 -0400

    Update license headers and year.

--- src/gaston_hilvl.jl
+++ src/gaston_hilvl.jl
@@ -1,29 +1,7 @@
-## Copyright (c) 2012 Miguel Bazdresch
+## Copyright (c) 2013 Miguel Bazdresch
 ##
-## Permission is hereby granted, free of charge, to any person obtaining a
-## copy of this software and associated documentation files (the "Software"),
-## to deal in the Software without restriction, including without limitation
-## the rights to use, copy, modify, merge, publish, distribute, sublicense,
-## and/or sell copies of the Software, and to permit persons to whom the
-## Software is furnished to do so, subject to the following conditions:
-##
-## The above copyright notice and this permission notice shall be included in
-## all copies or substantial portions of the Software.
-##
-## THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-## IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-## FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-## AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-## LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-## FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-## DEALINGS IN THE SOFTWARE.
+## This file is distributed under the 2-clause BSD License.
 
-## This file contains "high-level" plotting functions, similar to Octave's.
-
-# Closes a figure, or current figure if not given any arguments.
-# If handle is not empty, and it's not a natural number, errors out.
-# If handle is a natural number but no such figure exists, returns 0.
-# Otherwise, returns the handle of the figure that was closed.
 function closefigure(x...)
     global gnuplot_state
     global gaston_config
