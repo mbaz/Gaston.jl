2b135f0a928b939706b41d66f81f423c95fd0db8
Michal Sojka <michal.sojka@cvut.cz>
2019-04-04 02:01:51 +0200

    Rework fetching of gnuplot output
    The new code is more robust, faster and simpler.
    
    The old code could deadlock when gnuplot sent large SVGs to stdout, as
    was happening at least in Jupyter notebooks. The deadlock occurred
    because the ChanStdOut had limited capacity. When both the channel and
    OS kernel pipe buffer were full, gnuplot blocked waiting for Gaston to
    consume the output. But this never happened, because Gaston was
    waiting for "GastonDone" on stderr, which gnuplot should have sent
    after sending the whole figure. But gnuplot was blocked...
    
    Now, gnuplot's output is consumed all the time during plotting.
    
    We also try to detect better the start and end of the sent data by
    inserting markers to both stdout and stderr streams and only process
    the data between those markers.
    
    The new code is faster, because it does not use sleeps to wait for
    gnuplot. If gnuplot is able to supply the data quickly, we consume
    them immediately and finish just when we see the end marker, without
    additional delays.
    
    On the other hand, when gnuplot is slow, we just wait in
    readavailable(). We use timeout timer to handle the case when the end
    marker does not arrive for whatever reason in reasonable time.
    
    Fixes #103

--- src/gaston_init.jl
+++ src/gaston_init.jl
@@ -4,32 +4,6 @@
 
 # Set up pipes and start gnuplot process
 
-# Async tasks to read/write to gnuplot's pipes.
-const StartTasks = Condition()  # signal to start reading pipes
-
-const ChanStdOut = Channel(10)
-const ChanStdErr = Channel(10)
-
-# This task reads all characters available from gnuplot's stdout.
-@async begin
-    wait(StartTasks)
-    while true
-        stdoutstr = String(readavailable(gstdout))
-        process_running(gproc) || break
-        put!(ChanStdOut, stdoutstr)
-    end
-end
-
-# This task reads all characters available from gnuplot's stderr.
-@async begin
-    wait(StartTasks)
-    while true
-        stderrstr = String(readavailable(gstderr))
-        process_running(gproc) || break
-        put!(ChanStdErr, stderrstr)
-    end
-end
-
 # initialize pipes and run gnuplot
 gstdin = Pipe()
 gstdout = Pipe()
@@ -41,6 +15,3 @@ process_running(gproc) || error("There was a problem starting up gnuplot.")
 close(gstdout.in)
 close(gstderr.in)
 close(gstdin.out)
-
-# Start tasks to read and write gnuplot's pipes
-NTasks = notify(StartTasks)
--- src/gaston_llplot.jl
+++ src/gaston_llplot.jl
@@ -54,6 +54,18 @@ function llplot()
     # Reset gnuplot settable options.
     gnuplot_send("\nreset\n")
 
+    # Start reading gnuplot's stdout in "background"
+    ch_out = async_reader(gstdout, 5)
+
+    # In order to capture all output produced by our plotting commands
+    # (error messages and figure text in case of text terminals), we
+    # send "marks" to the stdout and stderr streams before the first
+    # and after the last command. Everything between these marks will
+    # be returned by our async_readers.
+    gnuplot_send("set print \"-\"") # Redirect print to stdout
+    gnuplot_send("print \"GastonBegin\"")
+    gnuplot_send("printerr \"GastonBegin\"")
+
     # Build terminal setup string and send it to gnuplot
     gnuplot_send(termstring())
 
@@ -176,61 +188,32 @@ function llplot()
     gnuplot_state.gp_lasterror = err
     gnuplot_state.gp_error = false
 
-    attempt_stderr = 20
-    attempt_stdout = 100
-    sleep_interval = 0.05
-    sleep_increment = 1.2
+    gnuplot_send("print \"GastonDone\"")
+    gnuplot_send("printerr \"GastonDone\"")
 
-    gnuplot_send("printerr \"GastonDone\"\n")
-    sleep(sleep_interval)
+    out = take!(ch_out)
 
-    # wait for stderr channel to be ready
-    si = sleep_interval
-    count = 0
-    while !isready(ChanStdErr)
-        sleep(si)
-        si = sleep_increment * si
-        count = count + 1
-        count > attempt_stderr &&
-        error("Gnuplot is taking too long to respond.")
-    end
+    ch_err = async_reader(gstderr, 1)
+    err = take!(ch_err)
 
-    # read all data in channel
-    si = sleep_interval
-    while isready(ChanStdErr)
-        err = err * take!(ChanStdErr)
-        sleep(si)
-        si = sleep_increment * si
-    end
+    out === :timeout && error("Gnuplot is taking too long to respond.")
+    out === :eof     && error("Gnuplot crashed")
+
+    # We don't care about stderr timeouts.
+    err === :timeout && (err = "")
+    err === :eof     && (err = "")
 
     # check for errors while plotting
-    if err != "GastonDone\n"
+    if err != ""
         gnuplot_state.gp_lasterror = err
         gnuplot_state.gp_error = true
         @warn("Gnuplot returned an error message:\n  $err)")
     end
 
     # if there was no error and text terminal, read all data from stdout
-    if err == "GastonDone\n"
+    if err == ""
         if (gaston_config.terminal ∈ term_text)
-            # wait for stdout to be ready
-            si = sleep_interval
-            count = 0
-            while !isready(ChanStdOut)
-                sleep(si)
-                si = sleep_increment * si
-                count = count + 1
-                count > attempt_stdout &&
-                error("Gnuplot is taking too long to respond.")
-            end
-            svgdata = ""
-            si = sleep_interval
-            while isready(ChanStdOut)
-                svgdata = svgdata * take!(ChanStdOut)
-                sleep(si)
-                si = sleep_increment * si
-            end
-            fig.svg = svgdata
+            fig.svg = out
         end
     end
 
