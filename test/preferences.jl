cbc975128769e40de0dc5c381695a75d892c68e3
t-bltg <tf.bltg@gmail.com>
2025-07-01 09:43:46 +0200

    update

--- test/preferences.jl
+++ test/preferences.jl
@@ -2,7 +2,7 @@ using Preferences, Gaston
 
 PREVIOUS_DEFAULT_GNUPLOT = load_preference(Gaston, "gnuplot_binary")
 
-@testset "Preferences" begin
+@testset "Invalid path" begin
     script = tempname()
     set_preferences!(Gaston, "gnuplot_binary" => "/some_non_existent_invalid_path"; force = true)
     rm.(Base.find_all_in_cache_path(Base.PkgId(Gaston, string(nameof(Gaston)))))  # make sure the compiled cache is removed
@@ -12,6 +12,24 @@ PREVIOUS_DEFAULT_GNUPLOT = load_preference(Gaston, "gnuplot_binary")
         using Gaston, Test
         res = @testset "[subtest] invalid gnuplot_binary path" begin
             @test Gaston.gnuplot_cmd ≡ nothing
+        end
+        exit(res.n_passed == 1 ? 0 : 123)
+        """
+    )
+    @test run(```$(Base.julia_cmd()) $script```) |> success
+end
+
+@testset "System gnuplot" begin
+    script = tempname()
+    sys_gnuplot = Sys.which("gnuplot")
+    set_preferences!(Gaston, "gnuplot_binary" => sys_gnuplot; force = true)
+    rm.(Base.find_all_in_cache_path(Base.PkgId(Gaston, string(nameof(Gaston)))))  # make sure the compiled cache is removed
+    write(
+        script,
+        """
+        using Gaston, Test
+        res = @testset "[subtest] system gnuplot" begin
+            @test Gaston.gnuplot_cmd == Cmd(["$sys_gnuplot"])
         end
         exit(res.n_passed == 1 ? 0 : 123)
         """
