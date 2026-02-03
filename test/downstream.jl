b967577bbb8a0ba35202bb18605c584b74e94ab9
t-bltg <tf.bltg@gmail.com>
2026-02-03 22:18:29 +0100

    indent

--- test/downstream.jl
+++ test/downstream.jl
@@ -1,23 +1,23 @@
 using Downloads, JSON, Test
 
 function available_channels()
-  juliaup = "https://julialang-s3.julialang.org/juliaup"
-  for i ∈ 1:6
-    buf = PipeBuffer()
-    Downloads.download("$juliaup/DBVERSION", buf)
-    dbversion = VersionNumber(readline(buf))
-    dbversion.major == 1 || continue
-    buf = PipeBuffer()
-    Downloads.download(
-        "$juliaup/versiondb/versiondb-$dbversion-x86_64-unknown-linux-gnu.json",
-        buf,
-    )
-    json = JSON.parse(buf)
-    haskey(json, "AvailableChannels") || continue
-    return json["AvailableChannels"]
-    sleep(10i)
-  end
-  return
+    juliaup = "https://julialang-s3.julialang.org/juliaup"
+    for i ∈ 1:6
+        buf = PipeBuffer()
+        Downloads.download("$juliaup/DBVERSION", buf)
+        dbversion = VersionNumber(readline(buf))
+        dbversion.major == 1 || continue
+        buf = PipeBuffer()
+        Downloads.download(
+            "$juliaup/versiondb/versiondb-$dbversion-x86_64-unknown-linux-gnu.json",
+            buf,
+        )
+        json = JSON.parse(buf)
+        haskey(json, "AvailableChannels") || continue
+        return json["AvailableChannels"]
+        sleep(10i)
+    end
+    return
 end
 
 """
@@ -25,22 +25,22 @@ julia> is_latest(:lts)
 julia> is_latest(:release)
 """
 function is_latest(variant)
-  channels = available_channels()
-  ver = let var::String = (
-      release = "release",
-      rel = "release",
-      lts = "lts",
-      release_candidate = "rc",
-      alpha = "alpha",
-      beta = "beta",
-      rc = "rc",
-  )[variant]
-      VersionNumber(split(channels[var]["Version"], '+') |> first)
-  end
-  dev = occursin("DEV", string(VERSION))  # or length(VERSION.prerelease) < 2
-  return !dev && (
-      VersionNumber(ver.major, ver.minor, 0, ("",)) ≤ VERSION < VersionNumber(ver.major, ver.minor + 1)
-  )
+    channels = available_channels()
+    ver = let var::String = (
+        release = "release",
+        rel = "release",
+        lts = "lts",
+        release_candidate = "rc",
+        alpha = "alpha",
+        beta = "beta",
+        rc = "rc",
+    )[variant]
+        VersionNumber(split(channels[var]["Version"], '+') |> first)
+    end
+    dev = occursin("DEV", string(VERSION))  # or length(VERSION.prerelease) < 2
+    return !dev && (
+        VersionNumber(ver.major, ver.minor, 0, ("",)) ≤ VERSION < VersionNumber(ver.major, ver.minor + 1)
+    )
 end
 
 (is_ci() && Sys.islinux() && is_latest(:release)) && @testset "downstream" begin
