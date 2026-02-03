ce6327625ce027dacc48086c0176e75e6b0d1408
t-bltg <tf.bltg@gmail.com>
2026-02-03 22:18:53 +0100

    ind

--- test/downstream.jl
+++ test/downstream.jl
@@ -64,14 +64,14 @@ end
 
         prefix = tempname()
         @time for i ∈ 1:length(PlotsBase._examples)
-          i ∈ PlotsBase._backend_skips[:gaston] && continue  # skip unsupported examples
-          PlotsBase._examples[i].imports ≡ nothing || continue  # skip examples requiring optional test deps
-          pl = PlotsBase.test_examples(:gaston, i; disp = false)
-          for ext in (".png", ".pdf")  # TODO: maybe more ?
-            fn = string(prefix, i, ext)
-            PlotsBase.savefig(pl, fn)
-            @test filesize(fn) > 1_000
-          end
+            i ∈ PlotsBase._backend_skips[:gaston] && continue  # skip unsupported examples
+            PlotsBase._examples[i].imports ≡ nothing || continue  # skip examples requiring optional test deps
+            pl = PlotsBase.test_examples(:gaston, i; disp = false)
+            for ext in (".png", ".pdf")  # TODO: maybe more ?
+                fn = string(prefix, i, ext)
+                PlotsBase.savefig(pl, fn)
+                @test filesize(fn) > 1_000
+            end
         end
         """
     )
